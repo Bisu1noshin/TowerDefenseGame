d126f5ec371087fa1ff0e9bfbef3e93883f19c14
Bisu1noshin <24ci0211@jec.ac.jp>
2025-06-04 01:27:44 +0900

    no message

---
+++ TowerDefenseGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInput inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInput();
+
+        inputActions.Player.Move.started += OnFire;
+
+        inputActions.Enable();
+    }
+
+    // 入力イベント
+
+    public void OnFire(InputAction.CallbackContext context) { 
+
+
+    }
+}
