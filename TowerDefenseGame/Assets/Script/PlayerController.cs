ea0943723508cfc091acbe1fe9ba0dc82ed11428
A06 <24ci0204@jec.ac.jp>
2025-06-04 15:55:16 +0900

    プレイヤーの基本部分を作成

--- TowerDefenseGame/Assets/Script/PlayerController.cs
+++ TowerDefenseGame/Assets/Script/PlayerController.cs
@@ -6,24 +6,49 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     PlayerInput inputActions;
+    float cnt_MouseTime = 0; //クリックしている時間をカウント
+    [SerializeField] float maxChargeTime = 5.0f; //最大チャージ時間
+    [SerializeField]GameObject bulletPrefab;
+    bool pushing = false; //マウスボタンがdown状態かのフラグ
 
     private void Awake()
     {
         inputActions = new PlayerInput();
 
-        inputActions.Player.Move.started += OnFire;
+        //入力処理関数をバインド
+        inputActions.Player.Shot.started += StartCharge;
+        inputActions.Player.Shot.canceled += Shoot;
 
         inputActions.Enable();
     }
 
+    private void Update()
+    {
+        if (pushing == true)
+        {
+            //チャージ
+            cnt_MouseTime += Time.deltaTime;
+            if( cnt_MouseTime > maxChargeTime ) { cnt_MouseTime=maxChargeTime; }
+        }
+    }
+
     // 入力イベント
 
-    public void OnFire(InputAction.CallbackContext context) { 
-
-
+    //クリック開始処理
+    public void StartCharge(InputAction.CallbackContext context)
+    {
+        pushing = true;
     }
 
-    public void OnExFire(InputAction.CallbackContext context) { 
+    //クリック終了処理(離したとき)
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        //弾を生成
+        GameObject shot=Instantiate(bulletPrefab);
 
+        //チャージのリセット
+        pushing = false;
+        cnt_MouseTime = 0;
     }
+    
 }
