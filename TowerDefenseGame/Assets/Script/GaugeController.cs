ccfb540c039602c8745880b76d4dcc93d66ff737
sungmin <sungmin-0203@github.com>
2025-06-04 16:35:16 +0900

    来週まで頑張ります！＾＾

---
+++ TowerDefenseGame/Assets/Script/GaugeController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeController : MonoBehaviour
+{
+    public float maxPower = 10f;
+    public float chargeSpeed = 2f;
+    public float dischargeSpeed = 2f;
+    private Slider powerSlider;
+
+    public float currentPower;
+
+    private Coroutine Charge;
+    private Coroutine disCharge;
+    private bool isCharing;
+
+
+    public void Start()
+    {
+        if(powerSlider != null)
+        {
+            powerSlider.minValue = 0f;
+            powerSlider.maxValue = maxPower;
+
+        }
+
+    }
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Charge());
+        }
+        else
+        {
+            StopCoroutine(Charge());
+        }
+    }
+
+    IEnumerator Charge()   //ゲージチャージ
+    {
+        while(isCharing == true)
+        {
+            powerSlider.value += Time.deltaTime * chargeSpeed;
+            yield return null;
+        }
+
+        while(isCharing == false)
+        {
+            powerSlider.value -= Time.deltaTime * chargeSpeed;
+            yield return null;
+        }
+    }
+}
