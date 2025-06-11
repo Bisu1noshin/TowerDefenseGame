8006d76aa4b809d84aa488acdb4a7160a5c2cd5f
24CI0210 <mizukikamedajp@gmail.com>
2025-06-11 15:19:35 +0900

    クマの挙動の詳細を追加

--- TowerDefenseGame/Assets/Prefab/Enemy/AttackRange_Script.cs
+++ TowerDefenseGame/Assets/Prefab/Enemy/AttackRange_Script.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class AttackRange_Script : MonoBehaviour
 {
-    bool colhit = false;
-    private void OnCollisionStay2D(Collision2D other)
+    bool colhit;
+    private void Start()
     {
-        if(other.gameObject.TryGetComponent<PlayerController>(out var p))
+        colhit = false;
+    }
+    private void Update()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
         {
             colhit = true;
         }
+        else
+        {
+            colhit = false;
+        }
     }
-    private void OnCollisionExit2D(Collision2D other)
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            colhit = true;
+        }
+        else
+        {
+            colhit = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
     {
         colhit = false;
     }
